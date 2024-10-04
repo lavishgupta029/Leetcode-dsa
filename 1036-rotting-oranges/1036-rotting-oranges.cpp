f8a643246b2b8691ec0babfe39664f329daa879a
lavishgupta029 <59661604+lavishgupta029@users.noreply.github.com>
2024-10-04 07:36:22 +0530

    Time: 0 ms (100%), Space: 16.8 MB (16.74%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -5,12 +5,12 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         int counter=0;
-        vector<vector<int>> visited(m,vector<int>(n,0));
+        // vector<vector<int>> visited(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
-                    visited[i][j]=1;
+                    // visited[i][j]=1;
                     
                 }
                 else if(grid[i][j]==1){
@@ -31,9 +31,10 @@ public:
             for(int i=0;i<4;i++){
                 int newRow=row+x[i];
                 int newCol=col+y[i];
-                if(newRow<m && newCol<n && newRow>=0 && newCol>=0 && visited[newRow][newCol]==0 && grid[newRow][newCol]==1){
+                if(newRow<m && newCol<n && newRow>=0 && newCol>=0 && grid[newRow][newCol]==1){
                     q.push({{newRow,newCol},t+1});
-                    visited[newRow][newCol]=1;
+                    // visited[newRow][newCol]=1;
+                    grid[newRow][newCol]=2;
                     counter--;
                 }
             }
