76b8cc6e3307d1d51b8cb3efd8e8008f6c77009f
lavishgupta029 <59661604+lavishgupta029@users.noreply.github.com>
2024-10-28 13:43:57 +0530

    Time: 2450 ms (10%), Space: 92.9 MB (85%) - LeetHub

--- 3608-find-the-number-of-subsequences-with-equal-gcd/3608-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/3608-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,32 +1,34 @@
+#include <algorithm> 
+
 class Solution {
-    const int mod = 1e9 + 7;
-    int dp[200][201][201];
 public:
-    int solve(int i, vector<int> &nums, int first, int second){
-        // first == gcd of first subsequence till now
-        // second = gcd of second subsequence till now
-
-        if(i == nums.size()){
-            return (first && second) && (first == second);
+    long long mod= 1e9+7;
+    long long dp[200][201][201];
+    long long helper(long long i,long long ga,long long gb, vector<int>& nums){
+        if(i==nums.size()){
+            if(ga==gb && ga!=0) return 1;
+            return 0;
         }
-        
-        if(dp[i][first][second] != -1) return dp[i][first][second];
-        
-        // Dont include this element in any subsequence
-        int skip = solve(i + 1, nums, first, second);
+        if(dp[i][ga][gb]!=-1) return dp[i][ga][gb];
+        long long ans=helper(i+1,ga,gb,nums);
+        if(ga==0){
+            ans += helper(i+1,nums[i],gb,nums);
+        }
+        else{
+            ans += helper(i+1,gcd(ga,nums[i]),gb,nums);
+        }
+        if(gb==0){
+            ans += helper(i+1,ga,nums[i],nums);
+        }
+        else{
+            ans += helper(i+1,ga,gcd(gb,nums[i]),nums);
+        }
 
-        // Include this index in the first subsequence
-        int take1 = solve(i + 1, nums, __gcd(first, nums[i]), second);
-
-        // Include this index in the second subsequence
-        int take2 = solve(i + 1, nums, first, __gcd(second, nums[i]));
-        
-        // Summing up all the possibilites
-        return dp[i][first][second] = (0LL + skip + take1 + take2) % mod;
+        return dp[i][ga][gb]=ans%mod;
     }
-    
     int subsequencePairCount(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, nums, 0, 0);
+        memset(dp,-1,sizeof(dp));
+        return helper(0,0,0,nums);
+
     }
 };
