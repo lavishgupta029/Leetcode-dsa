29f58ea783f084160b0693a14dada3f729444faa
lavishgupta029 <59661604+lavishgupta029@users.noreply.github.com>
2026-02-04 20:36:45 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) 
-    {
-        int x,y;
-        if(binary_search (nums.begin(), nums.end(), target))
-        {
-            x = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-            y = upper_bound(nums.begin(), nums.end(), target) - nums.begin()-1;
+
+    int leftMostElement(int n,vector<int>& nums, int target ){
+        int l=0,r=n-1;
+        int left_most=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>target) r=mid-1;
+            else if (nums[mid]<target) l=mid+1;
+            else{
+                left_most=mid;
+                r=mid-1;
+            }
         }
-        else 
-        {
-            return{-1,-1};
+        return left_most;
+    }
+
+    int rightMostElement(int n,vector<int>& nums, int target ){
+        int l=0,r=n-1;
+        int right_most=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>target) r=mid-1;
+            else if (nums[mid]<target) l=mid+1;
+            else{
+                right_most=mid;
+                l=mid+1;
+            }
         }
-        return {x,y};
+        return right_most;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int left_most=leftMostElement(n,nums,target);
+        int right_most=rightMostElement(n,nums,target);
+        return {left_most,right_most};
     }
 };
