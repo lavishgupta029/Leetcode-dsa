d2b04d362001592b5ab3679cd7785d16fcb8306b
lavishgupta029 <59661604+lavishgupta029@users.noreply.github.com>
2026-04-20 19:10:30 +0530

    Time: 10 ms (24.89%), Space: 20.4 MB (14.83%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -13,19 +13,42 @@ public:
         path[u]=false;
         return false;
     }
+   
+    bool bfs(vector<vector<int>> &adj, vector<int> &inDegree){
+         int count=0;
+        queue<int> q;
+        for(int i=0;i<adj.size();i++ ){
+            if(inDegree[i]==0) q.push(i);
+        }
+
+        while(!q.empty()){
+            count++;
+            int u=q.front();
+            q.pop();
+
+            for(auto &v: adj[u]){
+                inDegree[v]--;
+                if(inDegree[v]==0) q.push(v);
+            }
+        }
+        return count==adj.size();
+
+
+    }
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(n);
-
+        vector<int> inDegree(n);
         for(auto v: prerequisites){
             adj[v[1]].push_back(v[0]);
+            inDegree[v[0]]++;
         }
         vector<int> visited(n,false);
         vector<int> path(n,false);
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                if(dfs(i,visited,path,adj)) return false;
-            }
-        }
-        return true;
+        // for(int i=0;i<n;i++){
+        //     if(!visited[i]){
+        //         if() return false;
+        //     }
+        // }
+        return bfs(adj, inDegree);
     }
 };
