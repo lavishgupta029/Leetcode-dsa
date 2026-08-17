db1e3ec37bcb6fb3ee481f82a2b4f9a7dc7105b8
lavishgupta029 <59661604+lavishgupta029@users.noreply.github.com>
2026-08-17 14:19:19 +0530

    Time: 7 ms (43.21%), Space: 134.3 MB (82.85%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        vector<int> result(nums.size(),-1);
-        if(k==0) return nums;
         int n=nums.size();
-        if(2*k+1>n) return result ; 
-        long long preCalc=0;
-        for(int i=0;i<=2*k;i++){
-            preCalc += nums[i];
-        }
-        cout<<preCalc<< "\n";
-        for(int i=k;i<n-k;i++){
-            if(i!=k){
-                preCalc -= nums[i-k-1];
-                preCalc += nums[i+k];
+        vector<int> result(n,-1);
+        long long sum=0;
+        int i=0,j=0;
+        while(j<n){
+            sum += nums[j];
+            int range=2*k+1;
+            if(j-i+1 == range){
+                result[j-k]=sum/range;
+                sum -=  nums[i];
+                i++;
             }
-            cout<<preCalc<< "\t";
-            result[i]=preCalc/(2*k+1);
+            j++;
         }
         return result;
-
-
     }
+
 };
